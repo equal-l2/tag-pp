db56b1364f5d5fa6b4c66f4cf7b0e09ce1531f80
equal-l2 <eng.equall2@gmail.com>
2019-07-17 08:15:00 +0900

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,34 @@ use std::io::Write;
 use std::prelude::v1::*;
 use tag_geotag::*;
 
+/// Preprocess tag.csv
+///
+/// Each lines of the resulting CSV is defined as below:
+///
+/// <line> ::= <tag-name>,<ids-len>,<ids>
+/// <ids> ::= <id>{,<id>}
+///
+/// Note that <ids-len> is the count of <id> in <ids>, and curly brackets mean optional elements.
+///
+/// The resulting CSV has a "NO_TAG" entry at the first line, which contains ids without tag.
+///
 fn sc_tag_pp(tag: String, to: String) {
     let mut tags = HashMap::new();
     let mut no_tags = HashSet::new();
 
     {
         let f = std::fs::File::open(tag).unwrap();
-        let mut r = std::io::BufReader::new(f);
+        let r = std::io::BufReader::new(f);
 
         let tag_re = regex::Regex::new(r"^(\d+),(.*)$").unwrap();
 
-        let mut buf = String::new();
-
         // read all entries and put them into HashMap
-        while r.read_line(&mut buf).unwrap() != 0 {
-            if buf.ends_with('\n') {
-                buf.pop();
+        for s in r.lines() {
+            let mut s = s.unwrap();
+            if s.ends_with('\n') {
+                s.pop();
             }
-            if let Some(i) = tag_re.captures(&buf) {
+            if let Some(i) = tag_re.captures(&s) {
                 let id = i.get(1).unwrap().as_str().parse::<u64>().unwrap();
                 let key = i.get(2).unwrap().as_str();
                 if key.is_empty() {
@@ -30,9 +40,8 @@ fn sc_tag_pp(tag: String, to: String) {
                     tags.entry(key.to_owned()).or_insert_with(Vec::new).push(id);
                 }
             } else {
-                eprintln!("Ignored : {}", buf);
+                eprintln!("Ignored : {}", s);
             }
-            buf.clear();
         }
         eprintln!("Entries: {}", tags.len());
         eprintln!("NO_TAG len: {}", no_tags.len());
@@ -72,6 +81,21 @@ fn sc_tag_pp(tag: String, to: String) {
     }
 }
 
+/// Preprocess geotag.csv
+///
+/// This reads "NO_TAG" from tag_pp.csv, then read geotag.csv, remove any entries contained in
+/// "NO_TAG", finally transform URL into 2 elements.
+///
+/// Each lines of resulting csv is defined as below:
+///
+/// <line> ::= <id>,<time>,<latitude>,<longitude>,<server-num>,<url-part>
+///
+/// The original URL is in the form as below:
+/// <url> ::= http://farm<server-num>.static.flickr.com/<url-part>.jpg
+///
+/// We remove common parts and extract the distinct parts.
+/// By the transformation, we can reduce data length by around 30%.
+///
 fn sc_geotag_pp(tag: String, geotag: String, to: String) {
     // retrieve NO_TAG
     let no_tags: HashSet<u64> = {
@@ -87,22 +111,25 @@ fn sc_geotag_pp(tag: String, geotag: String, to: String) {
 
     eprintln!("tag read");
 
-    let geotag_re = regex::Regex::new(&format!(r"^(\d+),(.+),(.+),(.+),{}(\d){}(.+){}$", URL_PREFIX, URL_COMMON, URL_SUFFIX)).unwrap();
+    let geotag_re = regex::Regex::new(&format!(
+        r"^(\d+),(.+),(.+),(.+),{}(\d){}(.+){}$",
+        URL_PREFIX, URL_COMMON, URL_SUFFIX
+    ))
+    .unwrap();
 
     let f = std::fs::File::open(geotag).unwrap();
-    let mut r = std::io::BufReader::new(f);
+    let r = std::io::BufReader::new(f);
 
     let mut geotags = HashMap::new();
-    let mut buf = String::new();
-    while r.read_line(&mut buf).unwrap() != 0 {
-        if buf.ends_with('\n') {
-            buf.pop();
+    for s in r.lines() {
+        let mut s = s.unwrap();
+        if s.ends_with('\n') {
+            s.pop();
         }
-        if let Some(i) = geotag_re.captures(&buf) {
+        if let Some(i) = geotag_re.captures(&s) {
             let mut i = i.iter().skip(1);
             let id: u64 = i.next().unwrap().unwrap().as_str().parse().unwrap();
             if no_tags.contains(&id) {
-                buf.clear();
                 continue;
             }
 
@@ -125,9 +152,8 @@ fn sc_geotag_pp(tag: String, geotag: String, to: String) {
                 },
             );
         } else {
-            eprintln!("Ignored : {}", buf);
+            eprintln!("Ignored : {}", s);
         }
-        buf.clear();
     }
     eprintln!("Entries: {}", geotags.len());
 
